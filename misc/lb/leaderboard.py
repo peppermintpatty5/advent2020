10cb2cc6ab47d89ac5da22d733833e2b93e4a584
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2020-12-09 21:19:30 -0500

    Major code refactor for leaderboard
    Table now shows time pairs instead of star counts

--- misc/lb/leaderboard.py
+++ misc/lb/leaderboard.py
@@ -3,10 +3,13 @@
 import argparse
 import json
 import sys
-from datetime import datetime, timezone
+from datetime import datetime, timedelta
 from typing import Any
 
 import tabulate
+
+
+YEAR = 2020
 
 
 def dictMultiGet(d: dict, *keys: Any, default=None) -> Any:
@@ -23,44 +26,79 @@ def dictMultiGet(d: dict, *keys: Any, default=None) -> Any:
     return d
 
 
-def getStarTimes(stats: dict, day: int) -> dict:
+def getTimes(stats: dict, day: int) -> dict:
     """
-    Returns dictionary of star completion times, mapping member ID to a pair of
-    `datetime` objects. The pair corresponds to part 1 and 2 of each puzzle.
+    Returns dictionary of completion times, mapping each member ID to a pair of
+    `timedelta` objects. The pair corresponds to parts 1 and 2 of the puzzle.
 
-    A time will be `None` if there was no completion time.
+    If player did not complete the puzzle, time will be `None`.
     """
-    starTimes = {}
+    times = {}
+    dayStart = datetime(YEAR, 12, day, 5)  # hour=5 for EST timezone
 
     for memID, member in stats["members"].items():
         x = dictMultiGet(member, "completion_day_level", str(day))
-        t1 = dictMultiGet(x, "1", "get_star_ts")
-        t2 = dictMultiGet(x, "2", "get_star_ts")
+        ts1 = dictMultiGet(x, "1", "get_star_ts")
+        ts2 = dictMultiGet(x, "2", "get_star_ts")
 
-        starTimes[memID] = (
-            datetime.utcfromtimestamp(int(t1)) if t1 is not None else None,
-            datetime.utcfromtimestamp(int(t2)) if t2 is not None else None,
+        time1 = (
+            datetime.utcfromtimestamp(int(ts1)) - dayStart if ts1 is not None else None
         )
+        time2 = (
+            datetime.utcfromtimestamp(int(ts2)) - dayStart if ts2 is not None else None
+        )
+        times[memID] = (time1, time2)
 
-    return starTimes
+    return times
 
 
-def getPoints(starTimes: dict) -> dict:
+def getScores(times: dict) -> dict:
     """
-    Given a dictionary of star completion times, awards points based on the
-    order in which both parts were completed.
+    Returns dictionary of scores, mapping each member ID to a pair of integers.
+    The pair corresponds to parts 1 and 2 of the puzzle.
+
+    If player did not complete the puzzle, score will be `0`.
     """
-    points = {}
-    n = len(starTimes)
-    r1 = sorted(starTimes, key=lambda memID: starTimes[memID][0] or datetime.now())
-    r2 = sorted(starTimes, key=lambda memID: starTimes[memID][1] or datetime.now())
+    n = len(times)
+    ranks1 = sorted(
+        (memID for memID in times if times[memID][0] is not None),
+        key=lambda memID: times[memID][0],
+    )
+    ranks2 = sorted(
+        (memID for memID in times if times[memID][1] is not None),
+        key=lambda memID: times[memID][1],
+    )
 
-    for i, memID in enumerate(r1):
-        points[memID] = n - i if starTimes[memID][0] is not None else 0
-    for i, memID in enumerate(r2):
-        points[memID] += n - i if starTimes[memID][0] is not None else 0
+    scores1 = {}
+    scores2 = {}
+    for i, memID in enumerate(ranks1):
+        scores1[memID] = n - i
+    for i, memID in enumerate(ranks2):
+        scores2[memID] = n - i
 
-    return points
+    return {memID: (scores1.get(memID, 0), scores2.get(memID, 0)) for memID in times}
+
+
+def aocTimeFormat(td: timedelta) -> str:
+    """
+    Formats `td` like `H:MM:SS`, which is the default format. If `td` is
+    greater than 1 day, returns `>24h`.
+    """
+    return format(td) if td < timedelta(days=1) else ">24h"
+
+
+def getRankTbl(stats: dict, day: int) -> list:
+    """
+    Returns list of rows, ordered by score, where each row contains
+    `[memID, (time1, time2), (score1, score2)]`.
+    """
+    times = getTimes(stats, day)
+    scores = getScores(times)
+
+    return [
+        [memID, times[memID], scores[memID]]
+        for memID in sorted(scores, key=lambda memID: sum(scores[memID]), reverse=True)
+    ]
 
 
 def printDailyRankTbl(stats: dict, day: int) -> None:
@@ -68,22 +106,24 @@ def printDailyRankTbl(stats: dict, day: int) -> None:
     Prints formatted table containing ranking information for the particular
     day (non-cumulative).
     """
-    starTimes = getStarTimes(stats, day)
-    points = getPoints(starTimes)
+    times = getTimes(stats, day)
+    scores = getScores(times)
 
     tblRows = []
-    for i, memID in enumerate(
-        sorted(points, key=lambda memID: points[memID], reverse=True)
+    for i, [memID, (time1, time2), (score1, score2)] in enumerate(
+        getRankTbl(stats, day)
     ):
-        member = stats["members"][memID]
-        name = (
-            member["name"]
-            if member["name"] is not None
-            else f"(anonymous user #{memID})"
+        name = stats["members"][memID]["name"] or f"(anonymous user #{memID})"
+        f_time1 = aocTimeFormat(time1) if time1 is not None else "-"
+        f_time2 = aocTimeFormat(time2) if time2 is not None else "-"
+        tblRows.append([i + 1, score1, score2, f_time1, f_time2, name])
+
+    print(
+        tabulate.tabulate(
+            tblRows,
+            headers=["Rank", "Score 1", "Score 2", "Time 1", "Time 2", "Username"],
         )
-        stars = "".join("*" if x is not None else " " for x in starTimes[memID])
-        tblRows.append([i + 1, points[memID], stars, name])
-    print(tabulate.tabulate(tblRows, headers=["Rank", "Points", "Stars", "Username"]))
+    )
 
 
 def main():
