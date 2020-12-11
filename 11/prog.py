9c64a27db94d923a160b66d4e3b2c3839bfe52e6
Andrew Kritzler <10812727+peppermintpatty5@users.noreply.github.com>
2020-12-11 10:59:19 -0500

    Code cleanup

--- 11/prog.py
+++ 11/prog.py
@@ -1,18 +1,17 @@
 #!/usr/bin/env python3
 
+from itertools import product
+
+
+ADJ_OFFSETS = set(product({-1, 0, 1}, repeat=2)) - {(0, 0)}
+
 
 def countAdj(grid: list, r0: int, c0: int) -> int:
     count = 0
-    for dr in [-1, 0, 1]:
-        for dc in [-1, 0, 1]:
-            r, c = r0 + dr, c0 + dc
-            if (
-                (dr != 0 or dc != 0)
-                and 0 <= r < len(grid)
-                and 0 <= c < len(grid[0])
-                and grid[r][c] == "#"
-            ):
-                count += 1
+    for (dr, dc) in ADJ_OFFSETS:
+        r, c = r0 + dr, c0 + dc
+        if 0 <= r < len(grid) and 0 <= c < len(grid[0]) and grid[r][c] == "#":
+            count += 1
     return count
 
 
@@ -21,20 +20,13 @@ def stepA(grid: list) -> list:
     cols = len(grid[0])
     newGrid = []
 
-    adjGrid = []
-    for r in range(rows):
-        adjRow = []
-        for c in range(cols):
-            adjRow.append(countAdj(grid, r, c))
-        adjGrid.append(adjRow)
-
     for r in range(rows):
         newRow = []
         for c in range(cols):
             cell = grid[r][c]
-            if cell == "L" and adjGrid[r][c] == 0:
+            if cell == "L" and countAdj(grid, r, c) == 0:
                 newRow.append("#")
-            elif cell == "#" and adjGrid[r][c] >= 4:
+            elif cell == "#" and countAdj(grid, r, c) >= 4:
                 newRow.append("L")
             else:
                 newRow.append(cell)
@@ -56,41 +48,31 @@ def a():
 
 def countFar(grid: list, r0: int, c0: int) -> int:
     count = 0
-    origin = grid[r0][c0]
-    for dr in [-1, 0, 1]:
-        for dc in [-1, 0, 1]:
-            if dr != 0 or dc != 0:
-                r, c = r0 + dr, c0 + dc
-                while 0 <= r < len(grid) and 0 <= c < len(grid[0]):
-                    if grid[r][c] in "#L":
-                        if grid[r][c] == "#":
-                            count += 1
-                        break
-                    else:
-                        r += dr
-                        c += dc
+    for (dr, dc) in ADJ_OFFSETS:
+        r, c = r0 + dr, c0 + dc
+        while 0 <= r < len(grid) and 0 <= c < len(grid[0]):
+            if grid[r][c] in "#L":
+                if grid[r][c] == "#":
+                    count += 1
+                break
+            else:
+                r += dr
+                c += dc
     return count
 
 
 def stepB(grid: list) -> list:
     rows = len(grid)
     cols = len(grid[0])
-
-    adjGrid = []
-    for r in range(rows):
-        adjRow = []
-        for c in range(cols):
-            adjRow.append(countFar(grid, r, c))
-        adjGrid.append(adjRow)
-
     newGrid = []
+
     for r in range(rows):
         newRow = []
         for c in range(cols):
             cell = grid[r][c]
-            if cell == "L" and adjGrid[r][c] == 0:
+            if cell == "L" and countFar(grid, r, c) == 0:
                 newRow.append("#")
-            elif cell == "#" and adjGrid[r][c] >= 5:
+            elif cell == "#" and countFar(grid, r, c) >= 5:
                 newRow.append("L")
             else:
                 newRow.append(cell)
